a1a6cfb1e8854d4d94184dfbb26db07cba90a7b0
Araham Lagunas <araham.martinez@mail.concordia.ca>
2022-05-03 20:17:47 -0400

    Second Commit

---
+++ test2.py
@@ -0,0 +1,59 @@
+print("Hello World!")
+
+PI = 3.1416
+
+PI = 2
+
+#Multivariable Definition
+gender, age, name = "Male", "20", "Max"
+
+print(name)
+
+# Use parentheses when you use 2 lines of code
+sum = (1 +2 + 3 +
+4 +5+ 6+ 7)
+
+print(sum)
+
+#Variable Types
+my_integer_variable = 1000
+my_float_variable = 3.5
+my_complex_variable = 1+2j
+
+print(type(my_complex_variable))
+
+
+
+my_variable = 'This is a string variable!'
+first_word = my_variable.split()[0]
+print(first_word)
+
+x= 2**2
+print(x)
+print(5%2)
+
+text = ['Python', 'is', 'a', 'fun', 'programming', 'language']
+print(' '.join(text))
+
+txt3 = "My name is {}, I'm {}".format("John",36)
+print(txt3)
+
+Y = 'a' + 'b' + 'c'
+print(Y)
+
+a = "a" 
+
+print(a)
+
+Y= 5
+X= 4
+
+Z= X*Y
+
+print (Z)
+
+Div = Z/4
+
+print(Div)
+
+
